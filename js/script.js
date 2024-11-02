e29bc1fb93bf7f2e234c2d32dabe53048a72f885
xchen205 <xchen205@hawk.iit.edu>
2024-11-02 13:37:34 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ function init(){
 //add your javascrip between these two lines of code
   var textentry=entryinput;
   window.alert("Xing Chen, "+textentry);
-  h2.document.write("a");
-  window.addEventListener("click", init);
+  textoutput.document.write(textentry);
+  window.addEventListener("load", init);
 }

adeedcda09b88f4262bb8073aa51affed98b108d
xchen205 <xchen205@hawk.iit.edu>
2024-11-03 20:03:26 -0600

    does this work?

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function init(){
 //add your javascrip between these two lines of code
-  var textEntry=document.getElementById("entryinput");
+  var textEntry=document.getElementById("entryinput").innerHTML;
   window.alert("Xing Chen: "+textEntry);
   window.addEventListener("load", init);
   document.getElementById("textoutput").innerHTML=textEntry;

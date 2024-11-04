d3a5ebeedbae4244288cf2d051e560652e3f7e26
xchen205 <xchen205@hawk.iit.edu>
2024-11-03 20:17:29 -0600

    move

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-window.addEventListener("load", init);
-
 function init(){
 //add your javascrip between these two lines of code
   var textEntry=document.getElementById("entryinput").value;
   window.alert("Xing Chen: "+textEntry);
   document.getElementById("textoutput").innerHTML=textEntry;
 }
+
+window.addEventListener("load", init);
